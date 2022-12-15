0af473c9109b563e19bda666b3212e96cc26c80d
kylene-bright <krichter@brighthealthplan.com>
2022-12-14 20:51:14 -0600

    added data validation, readme

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# notable-take-home
+# Book A Doc API
+## Getting Started
+### Installation
+Clone repository and run `npm install` to install all dependencies.
+
+To run the server locally, run `node server.js` on the command line.
+
+## Available API Routes
+
+### GET /doctors
+Lists all doctors.
+
+### GET /doctors/{id}
+Gets a doctor by ID.
+
+### GET /doctors/{id}/appointments
+Lists all appointments for a specific doctor ID. Accepts a date query param for filtering.
+
+**Sample URL with Date Query**
+`http://localhost:3000/doctors/1/appointments?date=2018-05-09`
+
+### POST /appointments
+Creates an appointment from a JSON request body. Returns the created appointment ID.
+
+**Sample Request Body**
+```JSON
+{
+    "patientFirstName": "George",
+    "patientLastName": "Pup",
+    "startTime": 1671031800000,
+    "kind": "Follow-up",
+    "doctorId": 3
+}
+```
+
+### DELETE /appointments/{id}
+Deletes a specific appointment. Returns the deleted appointment ID.
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 const port = 3000
-const {data, APPOINTMENT_STATUS_ACTIVE, APPOINTMENT_STATUS_INACTIVE} = require("./utils/utils");
+const { data, APPOINTMENT_STATUS_ACTIVE, APPOINTMENT_STATUS_INACTIVE } = require("./utils/utils");
 
 app.use(express.json());
 
@@ -34,18 +34,44 @@ app.get('/doctors/:id/appointments', (req, res) => {
     res.send( matchingAppointments );
 });
 
-app.post('/appointments', (req, res) => {
-    let appointments = data["appointments"];
-    let newAppointment = req.body;
+app.get('/appointments', (req, res) => {
+    res.send( data["appointments"] );
+});
 
-    // TODO: data validation
+app.post('/appointments', (req, res) => {
+    const appointments = data["appointments"];
+    const newAppointment = req.body;
+
+    // check that appointment is at a 15-minute interval
+    const date = new Date(newAppointment["startTime"]);
+    if(date.getMinutes() % 15 !== 0) {
+        res.status(400);
+        res.send({
+            "message": "Appointment validation failed.",
+            "error": "Appointments must be on 15 minute intervals."
+        });
+    }
+
+    // check that doctor has less than 2 existing appointments scheduled at the requested time
+    // (doctor can have max 3 appointments for a given appointment time)
+    const existingAppointments = appointments.filter(appointment => {
+        return appointment.startTime === newAppointment.startTime && appointment.doctorId === newAppointment.doctorId;
+    })
+
+    if(existingAppointments.length > 2) {
+        res.status(400);
+        res.send({
+            "message": "Appointment validation failed.",
+            "error": "Doctor already has maximum number of appointments for requested appointment time."
+        });
+    }
 
     let newAppointmentId = Math.max(...appointments.map(o => o.id)) + 1
     newAppointment.id = newAppointmentId;
     newAppointment.status = APPOINTMENT_STATUS_ACTIVE;
     data["appointments"].push(newAppointment);
 
-    res.send({ id: newAppointmentId} );
+    res.send({ appointmentId: newAppointmentId} );
 });
 
 app.delete('/appointments/:id', (req, res) => {
@@ -55,9 +81,9 @@ app.delete('/appointments/:id', (req, res) => {
         return appointment.id === Number(req.params.id) ? {...appointment, status: APPOINTMENT_STATUS_INACTIVE} : appointment
     })
 
-    res.send({ id: req.params.id} );
+    res.send({ deletedAppointmentId: req.params.id} );
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Book A Doc API listening on port ${port}`)
 })
